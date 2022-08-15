ade764da6074e895fcc689577a4b02d4c99b8917
liaozhongxun <869664233@qq.com>
2022-08-15 15:49:31 +0800

    2022-08-15 15:49提交上传

---
+++ gitlist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 获取自己所有项目列表
+# -s 不输出下载进度
+# List=(`curl -H "Accept: application/vnd.github+json" -H "Authorization: token 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lzO" https://api.github.com/users/liaozhongxun/repos\?page\=1\&per_page\=2`)
+List=(`curl -H "Accept: application/vnd.github+json" https://api.github.com/users/liaozhongxun/repos\?page\=1\&per_page\=200`)
+
+Total=0
+for(( i=0; i<=${#List[@]}; i++ ))
+do
+    if [[ ${List[$i]} =~ ^\"https://github.com/liaozhongxun/.*\.git ]];then
+        Total=$[$Total+1]
+        echo ${List[$i]}
+    fi
+done
+
+echo "共 ${Total} 个"
